dbb685c0fab1b470b0b63c4d809ef83e377c9c04
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-05 16:58:26 +0300

    Add project files.

---
+++ Core_Proje.DataAccessLayer/EntityFramework/EfSocialMediaDal.cs
@@ -0,0 +1,15 @@
+﻿using Core_Proje.DataAccessLayer.Abstract;
+using Core_Proje.DataAccessLayer.Repository;
+using Core_Proje.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Proje.DataAccessLayer.EntityFramework
+{
+    public class EfSocialMediaDal:GenericRepository<SocialMedia>, ISocialMediaDal
+    {
+    }
+}
