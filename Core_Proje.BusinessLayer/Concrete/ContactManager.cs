4fabc2161cc4c97dbbc165215826ce4b9caa47ab
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-09 16:45:41 +0300

    Add project files.

---
+++ Core_Proje.BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,41 @@
+﻿using Core_Proje.BusinessLayer.Abstract;
+using Core_Proje.DataAccessLayer.Abstract;
+using Core_Proje.EntityLayer.Concrete;
+
+namespace Core_Proje.BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void TAdd(Contact entity)
+        {
+            _contactDal.Insert(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _contactDal.Delete(id);
+        }
+
+        public Contact TGetById(int id)
+        {
+            return _contactDal.GetById(id);
+        }
+
+        public List<Contact> TGetList()
+        {
+            return _contactDal.GetList();
+        }
+
+        public void TUpdate(Contact entity)
+        {
+            _contactDal.Update(entity);
+        }
+    }
+}
---
+++ Core_Proje.BusinessLayer/Concrete/ExperienceManager.cs
@@ -0,0 +1,49 @@
+﻿
+using Core_Proje.BusinessLayer.Abstract;
+using Core_Proje.DataAccessLayer.Abstract;
+using Core_Proje.DataAccessLayer.EntityFramework;
+using Core_Proje.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Proje.BusinessLayer.Concrete
+{
+    public class ExperienceManager : IExperienceService
+    {
+
+     private readonly IExperienceDal _experienceDal;
+
+        public ExperienceManager(IExperienceDal experienceDal)
+        {
+            _experienceDal = experienceDal;
+        }
+
+        public void TAdd(Experience entity)
+        {
+            _experienceDal.Insert(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _experienceDal.Delete(id);
+        }
+
+        public Experience TGetById(int id)
+        {
+            return _experienceDal.GetById(id);
+        }
+
+        public List<Experience> TGetList()
+        {
+            return _experienceDal.GetList();
+        }
+
+        public void TUpdate(Experience entity)
+        {
+            _experienceDal.Update(entity);
+        }
+    }
+}
