4a8107932fe668b8f0b4937678e7a5b097f3d088
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-14 16:20:03 +0300

    Add project files.

---
+++ Core_Proje.BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core_Proje.BusinessLayer.Abstract;
+using Core_Proje.DataAccessLayer.Abstract;
+using Core_Proje.EntityLayer.Concrete;
+
+namespace Core_Proje.BusinessLayer.Concrete
+{
+    public class FeatureManager : IGenericService<Feature>
+    {
+        private readonly IFeatureDal _featureDal;
+
+        public FeatureManager(IFeatureDal featureDal)
+        {
+            _featureDal = featureDal;
+        }
+
+        public void TAdd(Feature entity)
+        {
+            _featureDal.Insert(entity);
+        }
+
+        public void TDelete(int id)
+        {
+           _featureDal.Delete(id);
+           
+        }
+
+        public Feature TGetById(int id)
+        {
+           return _featureDal.GetById(id);
+        }
+
+        public List<Feature> TGetList()
+        {
+            return _featureDal.GetList();
+        }
+
+        public void TUpdate(Feature entity)
+        {
+             _featureDal.Update(entity);
+        }
+    }
+}
--- Core_Proje.WebUI/ViewComponents/Feature/FeatureList.cs
+++ Core_Proje.WebUI/ViewComponents/Feature/FeatureList.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core_Proje.BusinessLayer.Concrete;
+using Core_Proje.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.WebUI.ViewComponents.Feature
 {
     public class FeatureList:ViewComponent
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = featureManager.TGetList();
+            return View(values);
         }
     }
 }
