796db27461113576a1df87620c255f74614f4539
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-21 00:31:37 +0300

    Add project files.

---
+++ Core_Proje.WebUI/ViewComponents/Script/ScriptList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.WebUI.ViewComponents.Script
+{
+    public class ScriptList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
