b39698789d11561a51f76f634b730f06b16994ee
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-23 23:23:59 +0300

    Add project files.

---
+++ Core_Proje.WebUI/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,16 @@
+﻿using Core_Proje.BusinessLayer.Concrete;
+using Core_Proje.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.WebUI.ViewComponents.Experience
+{
+    public class ExperienceList:ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
