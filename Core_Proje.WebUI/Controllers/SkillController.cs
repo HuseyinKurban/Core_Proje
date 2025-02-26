94329a9cc33e8f445f9f2eb95295b71e4b7ffb27
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-26 23:34:01 +0300

    Add project files.

--- Core_Proje.WebUI/Controllers/SkillController.cs
+++ Core_Proje.WebUI/Controllers/SkillController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core_Proje.BusinessLayer.Concrete;
+using Core_Proje.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.WebUI.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+
         public IActionResult Index()
         {
-            return View();
+            var values = skillManager.TGetList();
+            return View(values);
         }
     }
 }
