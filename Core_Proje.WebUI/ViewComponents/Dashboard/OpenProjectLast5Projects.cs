65802d4d0873131766b35d0a698acb725db6b77a
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-04-09 20:48:46 +0300

    Add project files.

--- Core_Proje.WebUI/ViewComponents/Dashboard/OpenProjectLast5Projects.cs
+++ Core_Proje.WebUI/ViewComponents/Dashboard/OpenProjectLast5Projects.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core_Proje.BusinessLayer.Concrete;
+using Core_Proje.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.WebUI.ViewComponents.Dashboard
 {
     public class OpenProjectLast5Projects : ViewComponent
     {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioManager.TGetList();
+            var deger=values.OrderByDescending(x=>x.PortfolioId).Take(5).ToList();
+            return View(deger);
         }
     }
 
